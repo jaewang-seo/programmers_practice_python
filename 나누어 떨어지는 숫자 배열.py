61c4c465b2429da62c35b60ca3b88d2f120fd4d0
Jaewang Seo <68671222+jaewang-seo@users.noreply.github.com>
2020-09-29 18:53:17 +0900

    Add files via upload

---
+++ 나누어 떨어지는 숫자 배열.py
@@ -0,0 +1,13 @@
+def solution(arr, divisor):
+    answer = []
+    for i in arr:
+        if i % divisor == 0:
+            answer.append(i)
+    answer = sorted(answer)
+
+    if len(answer) == 0:
+        answer.append(-1)
+
+    return answer
+
+solution([5, 9, 7, 10], 20)
